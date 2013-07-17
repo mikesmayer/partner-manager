0187e51cb797879d861d879ab0b002d80737bc24
Mike Mayer <mayerms@gmail.com>
2013-07-17 18:38:55 -0400

    Generate phone number and associate with person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  has_many :phone_numbers
+
   attr_accessible :first_name, :last_name
 
   validates :first_name, :last_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 PartnerManager::Application.routes.draw do
+  resources :phone_numbers
+
+
   resources :people
 
 
