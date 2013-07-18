d01892b770b0efe1ef16cb555745d61bada7107b
Mike Mayer <mayerms@gmail.com>
2013-07-18 08:25:50 -0400

    Added edit functionality, now starting destroy functionality

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -106,6 +106,10 @@ describe PhoneNumbersController do
 
   describe "PUT update" do
     describe "with valid params" do
+
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {number: '555-5678', person_id: bob.id} }
+
       it "updates the requested phone_number" do
         phone_number = PhoneNumber.create! valid_attributes
         # Assuming there are no other phone_numbers in the database, this
@@ -125,7 +129,7 @@ describe PhoneNumbersController do
       it "redirects to the phone_number" do
         phone_number = PhoneNumber.create! valid_attributes
         put :update, {:id => phone_number.to_param, :phone_number => valid_attributes}, valid_session
-        response.should redirect_to(phone_number)
+        expect(response).to redirect_to(bob)
       end
     end
 
