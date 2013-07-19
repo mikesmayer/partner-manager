fa6f473cebdfa59ba13eb759cdbb9825bd6908e8
Mike Mayer <mayerms@gmail.com>
2013-07-19 13:16:43 -0400

    updated stylesheets and implemented application, navigation and messages layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
 
   def new
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'PartnerTrack helps you find great local consultants and turn them into great partners.'
   end
 
 end
