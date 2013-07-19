32ce9499aa25fc7899010624a6bf310ef5d326fb
Mike Mayer <mayerms@gmail.com>
2013-07-19 14:21:55 -0400

    crated save data to a spreadsheet function

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -4,6 +4,20 @@ class Visitor < ActiveRecord::Base
   column :comment, :string
   validates_presence_of :favorite
 
-  IMAGE_LABELS = ['San Francisco', 'Sydney', 'Paris']
+  IMAGE_LABELS = ['Local partners to generate new leads and sales', 'Use partners to close leads you can\'t close yourself', 'Local consultants to provide after sales implementation, training and support services']
+
+  def update_spreadsheet
+    connection = GoogleDrive.login(ENV["GMAIL_USERNAME"], ENV["GMAIL_PASSWORD"])
+    ss = connection.spreadsheet_by_title('Rails-Bootstrap-Example')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Rails-Bootstrap-Example')
+    end
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    ws[last_row, 1] = Time.now
+    ws[last_row, 2] = self.favorite
+    ws[last_row, 3] = self.comment
+    ws.save
+  end
 
 end
